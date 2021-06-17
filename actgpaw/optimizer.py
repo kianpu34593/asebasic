ba440dae9eb8bfc8ce96a992610dc8c4070e8f01
Jiankun Pu <kianpu01@gmail.com>
2021-06-17 11:46:12 -0400

    update optimizer

--- actgpaw/optimizer.py
+++ actgpaw/optimizer.py
@@ -8,30 +8,28 @@ from fractions import Fraction
 import numpy as np
 from ase.dft.bee import BEEFEnsemble
 
-def optimize_bulk(atoms,step=0.05,fmax=0.01,location=' ',extname=' '):
+def optimize_bulk(atoms,step=0.05,fmax=0.01,location='',extname=''):
     cell=atoms.get_cell()
     name=atoms.get_chemical_formula(mode='hill')
     vol=atoms.get_volume()
     volumes=[]
     energies=[]
-    ## TO-DO: how to not output error when making directory
-    #os.makedirs(location)
-    #os.makedirs(location+'/'+'eos_fit')
     for x in np.linspace(1-2*step,1+2*step,5):
         atoms.set_cell(cell*x,scale_atoms=True)
         atoms.calc.set(txt=location+'/'+'eos_fit'+'/'+name+'_'+str(x)+'-'+extname+'.txt')
         energies.append(atoms.get_potential_energy())
         volumes.append(atoms.get_volume())
     eos=EquationOfState(volumes,energies,eos='birchmurnaghan')
-    v0,e0,B=eos.fit()
+    v0=eos.fit()[0]
     x0=(v0/vol)**Fraction('1/3')
     atoms.set_cell(x0*cell,scale_atoms=True)
-    atoms.calc.set(txt=location+'/'+name+'_'+str(x0)+'-'+extname+'.txt')
-    atoms.calc.attach(atoms.calc.write,5,location+'/'+name+'_'+str(x0)+'-'+extname+'.gpw')
-    dyn=BFGS(atoms=atoms,trajectory=location+'/'+name+'_'+str(x0)+'-'+extname+'.traj',
-            logfile=location+'/'+name+'_'+str(x0)+'-'+extname+'.log')
+    file_name=location+'/'+name+'-'+extname
+    atoms.calc.set(txt=file_name+'.txt')
+    # atoms.calc.attach(atoms.calc.write,5,file_name+'.gpw')
+    dyn=BFGS(atoms=atoms,trajectory=file_name+'.traj',
+            logfile=file_name+'.log')
     dyn.run(fmax=fmax)
-    atoms.calc.write(location+'/'+name+'_'+str(x0)+'-'+extname+'.gpw')
+    atoms.calc.write(file_name+'.gpw')
     ## TO-DO: add ensemble energies to file
 
 def surf_relax(surf, name, fmax=0.01, maxstep=0.04, replay_traj=None):

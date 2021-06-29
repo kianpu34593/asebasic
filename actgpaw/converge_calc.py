dd1befd01c27a17d144d84608d36625b5224941f
Jiankun Pu <kianpu01@gmail.com>
2021-06-29 12:49:08 -0400

    change the pbc

--- actgpaw/converge_calc.py
+++ actgpaw/converge_calc.py
@@ -75,8 +75,7 @@ class surf_calc_conv:
         ##read the smallest slab to get the kpoints
         self.ascend_all_cif_files_full_path=self.sort_raw_slab()
         raw_slab_smallest=read(self.ascend_all_cif_files_full_path[0])
-        print(raw_slab_smallest)
-        raw_slab_smallest=raw_slab_smallest.set_pbc([1,1,0])
+        raw_slab_smallest.pbc=[1,1,0]
         kpts=kdens2mp(raw_slab_smallest,kptdensity=kdensity,even=True)
         self.gpaw_calc.__dict__['parameters']['kpts']=kpts
         self.calc_dict=self.gpaw_calc.__dict__['parameters']

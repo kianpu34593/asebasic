e4390388c4936ae2f33352d962273017536b11d5
Jiankun Pu <kianpu01@gmail.com>
2021-08-02 15:00:52 -0400

    reset observer

--- actgpaw/optimizer.py
+++ actgpaw/optimizer.py
@@ -47,7 +47,7 @@ def relax(atoms, name, fmax=0.01, maxstep=0.04):
     slab_name=name+'/'+'slab'
     slab_hist_name=slab_name+'_history'
     atoms.calc.set(txt=slab_name+'.txt')
-    atoms.calc.__dict__['observers']={}
+    atoms.calc.__dict__['observers']=[]
     atoms.calc.attach(atoms.calc.write, 10, slab_name+"_interm.gpw")
 
     def _check_file_exists(filename):

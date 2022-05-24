ad361f51b90478874571c083ca8c2fe6f2f00daf
Jiankun Pu <kianpu01@gmail.com>
2022-05-24 16:28:39 +0800

    update generate

--- BASIC/example/generate_surface.py
+++ BASIC/example/generate_surface.py
@@ -5,4 +5,17 @@ element=f'{sys.argv[1]}_mp-{sys.argv[2]}'
 slab_facet_dict=generate_all_slab(element,
                 calculated=False,
                 max_ind=1)
-print(slab_facet_dict)
+facet_ls=[]
+shift_ls=[]
+order_ls=[]
+for facet,shift_temp_ls in slab_facet_dict.items():
+    order_temp_ls=[]
+    order_count=0
+    for shift in shift_temp_ls:
+        facet_ls.append(facet)
+        shift_ls.append(shift)
+        order_temp_ls.append(order_count)
+        order_count+=1
+    order_ls+=order_temp_ls
+
+print(facet_ls,shift_ls,order_ls)

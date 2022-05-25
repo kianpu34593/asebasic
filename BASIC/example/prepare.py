26fe8bc501cb5845b50c81d65618696e8a4b78f7
Jiankun Pu <kianpu01@gmail.com>
2022-05-25 15:38:31 +0800

    syntax update

--- BASIC/example/prepare.py
+++ BASIC/example/prepare.py
@@ -2,4 +2,4 @@ from BASIC.utils import create_init_dir, prepare_bulk_crystal_structure
 
 create_init_dir()
 prepare_bulk_crystal_structure(api_key="4exFmXgCO1GvzQ3w",
-                                materials_project_id='mp-29210')
+                                materials_project_id='mp-135')
--- BASIC/example/slab_single.py
+++ BASIC/example/slab_single.py
@@ -1,16 +1,16 @@
-from compute import slab_compute
+from BASIC.compute import slab_compute
 from gpaw import GPAW
 
 calc=GPAW(xc = 'PBE')
 converge_tuple=('single_compute','')
-computation_setting={'miller_plane':100,
-                    'shift':0,
-                    'order':0,
-                    'layer':'2_8'
-                    'vacuum':10
+computation_setting={'miller_plane':'100',
+                    'shift':'0.25',
+                    'order':'0',
+                    'layer':'2_4',
+                    'vacuum':10,
                     'fix_layer':2,
                     'fix_mode':'bottom',
-                    'surface_energy_calculation_mode':'DFT-bulk'
+                    'surface_energy_calculation_mode':'DFT-bulk',
                     'bulk_atom_energy':None
                     }
 
